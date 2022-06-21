f41e1a454fb7ffdd3b60e936f84347d12d38d3b2
bigbigking <2845758942@qq.com>
2022-06-21 15:57:26 +0800

    feat:生成token

--- modules/user/login.js
+++ modules/user/login.js
@@ -1,8 +1,17 @@
 const router = require('express').Router();
 const fs = require('fs');
 const path = require('path');
+const jwt = require('jsonwebtoken');
+const expressJWT = require('express-jwt');
+
+const secreKey = 'bigbigkingSecreKey'
+const expiresIn = '20s'
+
 router.post('/user/login',(req, res)=>{
-  console.log('req',req)
+  console.log('req',req.body)
+  let user = req.body
+  //生成token 不把密码加密进去
+  let token = jwt.sign({name:user.username},secreKey,{expiresIn})
   fs.readFile(path.join(__dirname, '../../json/user.json'),'utf8', (err, dataStr) => {
     if(err){
       res.send({
@@ -11,7 +20,8 @@ router.post('/user/login',(req, res)=>{
         msg:'读取文件失败'
       })
     }else{
-      let result = JSON.parse(dataStr).user[0];
+      let result = JSON.parse(dataStr).user[0]
+      result['token'] = token
       res.send({
         result,
         code:200,
