e6acbe1faa967cedd41291ebfc5102792de2733a
bigbigking <2845758942@qq.com>
2022-06-22 08:55:03 +0800

    feta:userinfo返回数据

--- modules/user/userinfo.js
+++ modules/user/userinfo.js
@@ -1,13 +1,22 @@
 const router = require('express').Router();
-
+const fs = require('fs');
+const path = require('path');
 router.get('/user/userinfo',(req, res)=>{
-  res.send({
-    result:{
-      username:'admin',
-      password:'123456'
-    },
-    code:200,
-    msg:'success'
+  fs.readFile(path.join(__dirname, '../../json/user.json'),'utf8', (err, dataStr) => {
+    if(err){
+      res.send({
+        result:{},
+        code:500,
+        msg:'读取文件失败'
+      })
+    }else{
+      let result = JSON.parse(dataStr).user[0]
+      res.send({
+        result,
+        code:200,
+        msg:'success'
+      }) 
+    }
   })
 })
 
